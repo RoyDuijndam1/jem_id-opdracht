a5bccc4c700a954b33697bb4fcde2aa43289a0f6
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-05-03 16:25:43 +0200

    make home page

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,27 @@
+import {useEffect, useState} from "react";
+import Card from "../components/Card";
+import Product from "../Classes/Product";
+import * as ProductApi from "../Connections/ProductApi"
+
+const Home = () => {
+    const [products, setProducts] = useState([])
+    const [query, setQuery] = useState("")
+    const [nameField, setNameField] = useState("")
+
+    useEffect(() => {
+        let query = "";
+        setQuery(query)
+    }, [nameField])
+
+    useEffect( () => {
+        (async () => setProducts(await ProductApi.get(10, 0)))()
+    }, [query])
+
+    return(
+        <div>
+            {products.map((product : Product, index: number) => <Card key={index} product={product}/>)}
+        </div>
+    )
+}
+
+export default Home;
