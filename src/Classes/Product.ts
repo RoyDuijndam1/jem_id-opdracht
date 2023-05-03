702e1e2c15dbffb98456852b6d14e9362564b387
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-05-03 16:25:00 +0200

    Make product class to contain products from api call

---
+++ src/Classes/Product.ts
@@ -0,0 +1,13 @@
+class Product{
+    constructor(public name: string,
+                public description: string,
+                public price: number,
+                public height: number,
+                public diameter: number,
+                public standingPlace: string,
+                public wateringNeeds: string,
+                public photoUrl: string) {
+    }
+}
+
+export default Product;
