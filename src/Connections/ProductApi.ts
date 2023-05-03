74d8d7c2ae69f4acf7a7d1de0b7913fab7e958ae
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-05-03 16:25:28 +0200

    make functions to do the api call

---
+++ src/Connections/ProductApi.ts
@@ -0,0 +1,17 @@
+const url = "https://jemid-warmupapi.azurewebsites.net/api/products"
+
+export async function get(pageSize: number, pageIndex: number) {
+    try{
+        return fetch(`${url}?pageSize=${pageSize}&pageIndex=${pageIndex}`)
+            .then(response => response.json())
+            .then(data => data.products)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getSingle(id: number) {
+    return fetch(`${url}/${id}`)
+        .then(response => response.json())
+        .then(data => data)
+}
