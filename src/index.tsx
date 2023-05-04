6af65e21f586fb95339038c953342bc0d16b61ac
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-05-04 23:09:23 +0200

    change router in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
+import router from "./routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <App/>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
