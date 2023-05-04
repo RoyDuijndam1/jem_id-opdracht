89a3d45737c9f06b2cda284583d1544387b1bfc1
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-05-04 12:03:46 +0200

    remove interface from card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,20 @@
 import Product from "../Classes/Product";
 
-
-interface CardProps {
-    product: Product
-}
-
 const Card = ({ product } : {product: Product}) => {
-    return <div className={"card"}>
-        <img className={"card__image"} src={product.photoUrl} alt={product.name}/>
-    <div>
-        <h5 className={"card__title"}>{product.name}</h5>
-        <ul>
-            <li className={"card__listItem"}>hoogte: {product.height}</li>
-            <li className={"card__listItem"}>diameter: {product.diameter}</li>
-            <li className={"card__listItem"}>prijs: {product.price}</li>
-            <li className={"card__listItem"}>standplaats: {product.standingPlace}</li>
-        </ul>
-    </div>
-</div>
+    return (
+        <div className={"card"}>
+            <img className={"card__image"} src={product.photoUrl} alt={product.name}/>
+            <div>
+                <h5 className={"card__title"}>{product.name}</h5>
+                <ul>
+                    <li className={"card__listItem"}>hoogte: {product.height}</li>
+                    <li className={"card__listItem"}>diameter: {product.diameter}</li>
+                    <li className={"card__listItem"}>prijs: {product.price}</li>
+                    <li className={"card__listItem"}>standplaats: {product.standingPlace}</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 
