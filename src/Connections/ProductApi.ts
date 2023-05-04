099aea253eb11915bea8030ef827cef1795801ac
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2023-05-04 23:11:48 +0200

    change the get to add a query

--- src/Connections/ProductApi.ts
+++ src/Connections/ProductApi.ts
@@ -1,8 +1,8 @@
 const url = "https://jemid-warmupapi.azurewebsites.net/api/products"
 
-export async function get(pageSize: number, pageIndex: number) {
+export async function get(pageSize: number, pageIndex: number, query: string) {
     try{
-        return fetch(`${url}?pageSize=${pageSize}&pageIndex=${pageIndex}`)
+        return fetch(`${url}?pageSize=${pageSize}&pageIndex=${pageIndex}&${query}`)
             .then(response => response.json())
             .then(data => data.products)
     } catch (error) {
